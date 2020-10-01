a822e31bbd22866ddd1ab26d620e601c4974084c
medved_v_ushanke <getingjagare@gmail.com>
2020-10-01 10:02:35 +0300

    moving data from tariffs table

--- app/Tariffs.php
+++ app/Tariffs.php
@@ -16,27 +16,11 @@ use Illuminate\Database\Eloquent\Model;
  * @property Categories $category
  * @property int $region_id
  * @property int $category_id
- * @property string $params
- * @property float $price_per_day
  * @property float $price
- * @property float $start_balance
- * @property string $unlimited
  */
 class Tariffs extends Model
 {
-    protected $fillable = ['name', 'description', 'image_link', 'params', 'price_per_day', 'price', 'start_balance', 'unlimited'];
-
-    public const PARAMS_LABELS = [
-        'sms' => 'СМС',
-        'gb' => 'Гб',
-        'min' => 'Минуты',
-        'start_balance' => 'Стартовый баланс',
-        'price_per_day' => 'Абонентская плата в сутки',
-        'whatsapp' => 'Безлимит на WhatsApp',
-        'viber' => 'Безлимит на Viber',
-        'skype' => 'Безлимит на Skype',
-        'network' => 'Безлимит внутри сети',
-    ];
+    protected $fillable = ['name', 'description', 'image_link', 'price'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
